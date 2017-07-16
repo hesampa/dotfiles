77192f89a0c619d64ec83632f180bbbb6963a31b
Hesam <hesamp@kth.se>
2017-07-17 01:30:17 +0200

    simpler symlink.sh

--- bin/symlink.sh
+++ bin/symlink.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 
-# used when GNU Stow is not installed
-# on the machine
+# Used when GNU Stow is not installed
+# on machine. symlink the essentials
 
-echo "Symlinking dotfiles..."
-
-if [ ! -d ~/dotfiles ]; then
-    echo "dotfiles folder not setup correctly. Exiting..."
-    exit 1
-else
-    cd ~/dotfiles
-fi
-
-files=("bash" "tmux.conf" "vim")
-
-for file in "${files[@]}"; do
-    if [[ $file == *"tmux"* ]]; then
-        ln -sf ~/dotfiles/tmux/.$file ~/.$file
-    else
-        ln -sf ~/dotfiles/$file/.${file}rc ~/.${file}rc
-    fi
-    echo "$file linked"
-done
-
-echo "Done!"
+ln -sf ~/dotfiles/bash/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/bash/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
