5b8e59bb346b5fbd83ddfca5928f632fcdf5d0e6
Hesam <hesamp@kth.se>
2017-07-17 13:40:17 +0200

    fixed path error

--- bin/symlink.sh
+++ bin/symlink.sh
@@ -7,6 +7,6 @@ mkdir -p ~/.vim/after
 
 ln -sf ~/dotfiles/bash/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/bash/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/vim/.vimrc/ ~/.vimrc
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim/.vim/after/ftplugin/ ~/.vim/after/
 ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
