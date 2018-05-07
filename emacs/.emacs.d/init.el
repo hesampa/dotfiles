a05ff22687fb5c6f74e150dfa0770a39b279b062
Hesam Pakdaman <hesamp@kth.se>
2018-05-07 08:42:55 +0200

    added init.el to emacs

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,26 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+(setq vc-follow-symlinks t)
+
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "emacs"
+                   user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ivy ## dmenu xterm-color hydra ace-window exwm multi-line magit python-mode linum-relative expand-region base16-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
