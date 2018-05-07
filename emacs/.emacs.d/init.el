0a59d5e632e0d6a1525c21e6eaff788295765274
Hesam Pakdaman <hesamp@kth.se>
2018-05-07 12:42:16 +0200

    cleanup

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -13,17 +13,3 @@
 (org-babel-load-file
  (expand-file-name "emacs"
                    user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (hydra ace-window exwm multi-line magit python-mode linum-relative expand-region base16-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
