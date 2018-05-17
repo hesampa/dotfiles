ddbfe902dce889b2e4d5e1c4f064ffbd1607942c
Hesam Pakdaman <hesamp@kth.se>
2018-05-17 08:35:27 +0200

    beacon and hydra for emacs alias fix for screenup/dwn

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,11 +1,18 @@
+(setq package-user-dir "~/.emacs.d/elpa")
+
 (package-initialize)
 (require 'package)
 (add-to-list 'package-archives
-    '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/"))
 
-(dolist (package '(use-package))
-   (unless (package-installed-p package)
-       (package-install package)))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 (setq vc-follow-symlinks t)
 
@@ -13,6 +20,7 @@
 (org-babel-load-file
  (expand-file-name "emacs"
                    user-emacs-directory))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -20,10 +28,11 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (paredit yasnippet-snippets yasnippet hydra use-package magit linum-relative ivy expand-region disable-mouse base16-theme))))
+    (py-autopep8 beacon ace-window jumplist smartparens paredit yasnippet-snippets yasnippet hydra use-package magit linum-relative ivy expand-region disable-mouse base16-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'erase-buffer 'disabled nil)
