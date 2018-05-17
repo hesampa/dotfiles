7f17dd73744e0b0d3550e788fb8f72cd7184bd5f
Hesam <hesamp@kth.se>
2018-05-17 21:42:23 +0200

    kill-buffer-and-window s-r, rm disable mouse

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -26,9 +26,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-term-color-vector
+   [unspecified "#1d1f21" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#81a2be" "#c5c8c6"])
  '(custom-safe-themes
    (quote
-    ("2c88b703cbe7ce802bf6f0bffe3edbb8d9ec68fc7557089d4eaa1e29f7529fe1" default)))
+    ("3380a2766cf0590d50d6366c5a91e976bdc3c413df963a0ab9952314b4577299" "2c88b703cbe7ce802bf6f0bffe3edbb8d9ec68fc7557089d4eaa1e29f7529fe1" default)))
  '(package-selected-packages
    (quote
     (doom-themes dmenu ivy-hydra disable-mouse use-package magit linum-relative ivy hydra expand-region beacon base16-theme))))
