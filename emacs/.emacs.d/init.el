0f22efc41a17cbcd5b0600914815ffa57d9ad5da
Hesam Pakdaman <hesamp@kth.se>
2018-05-17 10:45:36 +0200

    multi support

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -28,7 +28,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ivy-hydra disable-mouse use-package magit linum-relative ivy hydra expand-region beacon base16-theme))))
+    (dmenu ivy-hydra disable-mouse use-package magit linum-relative ivy hydra expand-region beacon base16-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
