d50f0f4ad5ecfe0106cf85460723a752b3389608
Hesam Pakdaman <hesamp@kth.se>
2018-05-17 10:17:12 +0200

    hydra-ivy and beacon

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,19 +1,13 @@
-(setq package-user-dir "~/.emacs.d/elpa")
-
-(package-initialize)
 (require 'package)
+(package-initialize)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/"))
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives
-             '("org" . "http://orgmode.org/elpa/"))
+             '("org" . "http://orgmode.org/elpa/") t)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
-;; update the package metadata is the local cache is missing
-(unless package-archive-contents
-  (package-refresh-contents))
 
 (dolist (package '(use-package))
    (unless (package-installed-p package)
@@ -26,3 +20,18 @@
 (org-babel-load-file
  (expand-file-name "emacs"
                    user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ivy-hydra disable-mouse use-package magit linum-relative ivy hydra expand-region beacon base16-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
