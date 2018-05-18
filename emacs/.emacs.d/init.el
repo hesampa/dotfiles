44a7b407ca3f8b3cafa374468f674428c77e4f08
Hesam Pakdaman <hesamp@kth.se>
2018-05-18 16:51:40 +0200

    changed back to base16 theme, better for term changed pwerline theme to default

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -32,7 +32,7 @@
     ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(package-selected-packages
    (quote
-    (paredit dmenu ivy-hydra disable-mouse use-package magit linum-relative ivy hydra expand-region beacon))))
+    (python-mode paredit dmenu ivy-hydra disable-mouse use-package magit linum-relative ivy hydra expand-region beacon))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
