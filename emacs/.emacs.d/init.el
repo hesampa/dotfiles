1663667cc935d615d6fda2ed91a917ad924ec040
Hesam <hesamp@kth.se>
2018-05-18 20:21:58 +0200

    rm custom-set-variables

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -20,22 +20,3 @@
 (org-babel-load-file
  (expand-file-name "emacs"
                    user-emacs-directory))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
- '(custom-safe-themes
-   (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
- '(package-selected-packages
-   (quote
-    (python-mode paredit dmenu ivy-hydra disable-mouse use-package magit linum-relative ivy hydra expand-region beacon))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
