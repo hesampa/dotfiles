2e1a7d2132b508d426f035a81e06db75ef674524
Hesam Pakdaman <hesamp@kth.se>
2018-07-02 11:29:42 +0200

    auto installation of some packs

---
+++ bin/setup.sh
@@ -0,0 +1,39 @@
+# install as root
+sudo -i
+
+# packs
+pacman -S adobe-source-code-pro-fonts
+pacman -S alsa-utils
+pacman -S arc-gtk-theme
+pacman -S awesome-terminal-fonts
+pacman -S calibre
+pacman -S compton
+pacman -S curl
+pacman -S dina-font
+pacman -S dtach
+pacman -S emacs
+pacman -S fakeroot
+pacman -S fasd
+pacman -S git
+pacman -S gtk3
+pacman -S gvim
+pacman -S linux-headers
+pacman -S lxappearance-gtk3
+pacman -S mpv
+pacman -S noto-emoji-fonts
+pacman -S noto-fonts
+pacman -S openssh
+pacman -S perl-rename
+pacman -S pulseaudio
+pacman -S python2
+pacman -S python3
+pacman -S ranger
+pacman -S rofi
+pacman -S stow
+pacman -S termite
+pacman -S thunar
+pacman -S ttf-dejavu
+pacman -S unrar
+pacman -S unzip
+pacman -S wget
+pacman -S zip
