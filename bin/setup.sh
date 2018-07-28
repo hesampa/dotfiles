f688367ee825e982601e377aeba9647cfcb273f9
Hesam Pakdaman <hesamp@kth.se>
2018-07-28 10:19:19 +0200

    removed pacman -S, use pacman's ability to install from file instead. Added some new pkgs.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,35 +1,49 @@
-# install as root
-sudo -i
-
-# packs
-pacman -S adobe-source-code-pro-fonts
-pacman -S adobe-source-sans-pro-fonts
-pacman -S adobe-source-serif-pro-fonts
-pacman -S alsa-utils
-pacman -S arc-gtk-theme
-pacman -S calibre
-pacman -S curl
-pacman -S dtach
-pacman -S emacs
-pacman -S fakeroot
-pacman -S fasd
-pacman -S git
-pacman -S gtk3
-pacman -S gvim
-pacman -S linux-headers
-pacman -S lxappearance-gtk3
-pacman -S mpv
-pacman -S openssh
-pacman -S pdfgrep
-pacman -S perl-rename
-pacman -S pulseaudio
-pacman -S python2
-pacman -S python3
-pacman -S stow
-pacman -S termite
-pacman -S thunar
-pacman -S ttf-dejavu
-pacman -S unrar
-pacman -S unzip
-pacman -S wget
-pacman -S zip
+adobe-source-code-pro-fonts
+adobe-source-sans-pro-fonts
+adobe-source-serif-pro-fonts
+alsa-utils
+arc-gtk-theme
+arc-icon-theme
+aspell-en
+bash
+calibre
+chromium
+cronie
+ctags
+curl
+dtach
+emacs
+fakeroot
+fasd
+feh
+firefox
+git
+grep
+gtk3
+gvim
+linux-headers
+lxappearance-gtk3
+mpv
+openssh
+pdfgrep
+perl-rename
+pulseaudio
+python
+python-pip
+python2
+python2-pip
+redshift
+ripgrep
+rsync
+sshfs
+stow
+sudo
+tar
+termite
+thunar
+ttf-dejavu
+unrar
+unzip
+wget
+which
+zip
