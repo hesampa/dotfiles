de734da9fc3adc03ec5209da2e316d9ed6310181
Hesam Pakdaman <hesamp@kth.se>
2020-02-17 10:31:24 +0100

    emacs: changed url for MELPA

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,7 +1,7 @@
 (require 'package)
 (package-initialize)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("org" . "http://orgmode.org/elpa/") t)
 
